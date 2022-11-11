756e2cbf461fd96a1919fccf8a64343d32a8bf3a
hurl365 <35495052+hurl365@users.noreply.github.com>
2022-11-11 11:53:56 -0800

    Fixed Bugs in Fetching

--- sw.js
+++ sw.js
@@ -41,21 +41,26 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
+  
   // B8. TODO - If the request is in the cache, return with the cached version.
   //            Otherwise fetch the resource, add it to the cache, and return
   //            network response.
-  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
-    // Respond with the image from the cache or from the network
-    return cache.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request).then((fetchedResponse) => {
-        // Add the network response to the cache for future visits.
-        // Note: we need to make a copy of the response to save it in
-        // the cache and use the original as the request response.
-        cache.put(event.request, fetchedResponse.clone());
+    event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+      // Go to the cache first
+      return cache.match(event.request).then((cachedResponse) => {
+        // Return a cached response if we have one
+        if (cachedResponse) {
+          return cachedResponse;
+        }
 
-        // Return the network response
-        return fetchedResponse;
+        // Otherwise, hit the network
+        return fetch(event.request).then((fetchedResponse) => {
+          // Add the network response to the cache for later visits
+          cache.put(event.request, fetchedResponse.clone());
+
+          // Return the network response
+          return fetchedResponse;
+        });
       });
-    });
-  }));
+    }));
 });
