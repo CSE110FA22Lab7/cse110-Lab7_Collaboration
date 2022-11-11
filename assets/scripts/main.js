940137c1987e714e8051786522cdaa1420b18616
GoldenStone929 <yshawncui@gmail.com>
2022-11-10 16:11:17 -0800

    expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -20,18 +20,18 @@ async function init() {
   // Get the recipes from localStorage
   let recipes;
   try {
-    recipes = await getRecipes();
+      recipes = await getRecipes();
   } catch (err) {
-    console.error(err);
+      console.error(err);
   }
   // Add each recipe to the <main> element
   addRecipesToDocument(recipes);
 }
 
 /**
- * Detects if there's a service worker, then loads it and begins the process
- * of installing it and getting it running
- */
+* Detects if there's a service worker, then loads it and begins the process
+* of installing it and getting it running
+*/
 function initializeServiceWorker() {
   // EXPLORE - START (All explore numbers start with B)
   /*******************/
@@ -57,73 +57,102 @@ function initializeServiceWorker() {
 }
 
 /**
- * Reads 'recipes' from localStorage and returns an array of
- * all of the recipes found (parsed, not in string form). If
- * nothing is found in localStorage, network requests are made to all
- * of the URLs in RECIPE_URLs, an array is made from those recipes, that
- * array is saved to localStorage, and then the array is returned.
- * @returns {Array<Object>} An array of recipes found in localStorage
- */
+* Reads 'recipes' from localStorage and returns an array of
+* all of the recipes found (parsed, not in string form). If
+* nothing is found in localStorage, network requests are made to all
+* of the URLs in RECIPE_URLs, an array is made from those recipes, that
+* array is saved to localStorage, and then the array is returned.
+* @returns {Array<Object>} An array of recipes found in localStorage
+*/
 async function getRecipes() {
   // EXPOSE - START (All expose numbers start with A)
   // A1. TODO - Check local storage to see if there are any recipes.
   //            If there are recipes, return them.
+
+  if (localStorage.getItem('recipes')) {
+      return JSON.parse(localStorage.getItem('recipes'));
+  }
+
   /**************************/
   // The rest of this method will be concerned with requesting the recipes
   // from the network
   // A2. TODO - Create an empty array to hold the recipes that you will fetch
+  let receiptList = [];
+  // for (let i = 0; i < localStorage.length; i++) {
+  //   receiptList.push(localStorage[i]);
+  // }
+
   // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
   //            has a great article on them. A promise takes one parameter - A
   //            function (we call these callback functions). That function will
   //            take two parameters - resolve, and reject. These are functions
   //            you can call to either resolve the Promise or Reject it.
-  /**************************/
-  // A4-A11 will all be *inside* the callback function we passed to the Promise
-  // we're returning
-  /**************************/
-  // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
-  //            declared above
-  // A5. TODO - Since we are going to be dealing with asynchronous code, create
-  //            a try / catch block. A6-A9 will be in the try portion, A10-A11
-  //            will be in the catch portion.
-  // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
-  //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
-  //            you must either use "await fetch(...)" or "fetch.then(...)". This
-  //            function is using the async keyword so we recommend "await"
-  // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
-  //            NOTE: .json() is ALSO asynchronous, so you will need to use
-  //            "await" again
-  // A8. TODO - Add the new recipe to the recipes array
-  // A9. TODO - Check to see if you have finished retrieving all of the recipes,
-  //            if you have, then save the recipes to storage using the function
-  //            we have provided. Then, pass the recipes array to the Promise's
-  //            resolve() method.
-  // A10. TODO - Log any errors from catch using console.error
-  // A11. TODO - Pass any errors to the Promise's reject() function
+
+  return new Promise(async function (resolve, reject) {
+      /**************************/
+      // A4-A11 will all be *inside* the callback function we passed to the Promise
+      // we're returning
+      /**************************/
+      // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
+      //            declared above
+      for (let i = 0; i < RECIPE_URLS.length; i++) {
+          // A5. TODO - Since we are going to be dealing with asynchronous code, create
+          //            a try / catch block. A6-A9 will be in the try portion, A10-A11
+          //            will be in the catch portion.
+          try {
+              // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
+              //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
+              //            you must either use "await fetch(...)" or "fetch.then(...)". This
+              //            function is using the async keyword so we recommend "await"
+              const urlList = await fetch(RECIPE_URLS[i]);
+              // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
+              //            NOTE: .json() is ALSO asynchronous, so you will need to use
+              //            "await" again
+              const urlFinal = await urlList.json();
+              // A8. TODO - Add the new recipe to the recipes array
+              receiptList.push(urlFinal);
+              // console.log(receiptList);
+              // A9. TODO - Check to see if you have finished retrieving all of the recipes,
+              //            if you have, then save the recipes to storage using the function
+              //            we have provided. Then, pass the recipes array to the Promise's
+              //            resolve() method.
+              if (RECIPE_URLS.length == receiptList.length) {
+                  saveRecipesToStorage(receiptList);
+                  resolve(receiptList);
+              }
+          } catch (error) {
+              // A10. TODO - Log any errors from catch using console.error
+              console.error(error);
+              // A11. TODO - Pass any errors to the Promise's reject() function
+              reject(error);
+          }
+      }
+  });
 }
 
+
 /**
- * Takes in an array of recipes, converts it to a string, and then
- * saves that string to 'recipes' in localStorage
- * @param {Array<Object>} recipes An array of recipes
- */
+* Takes in an array of recipes, converts it to a string, and then
+* saves that string to 'recipes' in localStorage
+* @param {Array<Object>} recipes An array of recipes
+*/
 function saveRecipesToStorage(recipes) {
   localStorage.setItem('recipes', JSON.stringify(recipes));
 }
 
 /**
- * Takes in an array of recipes and for each recipe creates a
- * new <recipe-card> element, adds the recipe data to that card
- * using element.data = {...}, and then appends that new recipe
- * to <main>
- * @param {Array<Object>} recipes An array of recipes
- */
+* Takes in an array of recipes and for each recipe creates a
+* new <recipe-card> element, adds the recipe data to that card
+* using element.data = {...}, and then appends that new recipe
+* to <main>
+* @param {Array<Object>} recipes An array of recipes
+*/
 function addRecipesToDocument(recipes) {
   if (!recipes) return;
   let main = document.querySelector('main');
   recipes.forEach((recipe) => {
-    let recipeCard = document.createElement('recipe-card');
-    recipeCard.data = recipe;
-    main.append(recipeCard);
+      let recipeCard = document.createElement('recipe-card');
+      recipeCard.data = recipe;
+      main.append(recipeCard);
   });
 }
